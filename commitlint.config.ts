59a3b710571c7f46c71912b8dfe24fccd46c094c
rahulmandyal1 <rahulmandyal079@gmail.com>
2026-01-31 21:57:09 +0530

    tooling

---
+++ commitlint.config.ts
@@ -0,0 +1,15 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert'],
+    ],
+    'subject-max-length': [2, 'always', 72],
+  },
+};
+
+export default Configuration;
